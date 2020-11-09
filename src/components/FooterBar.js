989d5675ab1ca146a2360b6fe187686b79ae4dc3
rauldevilla <raul.devilla@gmail.com>
2020-11-09 15:34:08 -0500

    Set small screen buttons

--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button'
+import { FaHome } from 'react-icons/fa';
+import { FaCalendarDay } from 'react-icons/fa';
+import { FaUserCog } from 'react-icons/fa';
 
 //import Container from 'react-bootstrap/Container';
 //import Row from 'react-bootstrap/Row';
@@ -8,7 +12,17 @@ class FooterBar extends Component {
 
     render() {
         return (
-            <div id="footer-bar"> Footer Bar </div>
+            <div id="footer-bar">
+                <Button className="footer-bar-button" variant="outline-dark">
+                    <FaUserCog className="footer-bar-icon"/>
+                </Button>
+                <Button className="footer-bar-button" variant="outline-dark">
+                    <FaCalendarDay className="footer-bar-icon"/>
+                </Button>
+                <Button className="footer-bar-button" variant="outline-dark">
+                    <FaHome className="footer-bar-icon"/>
+                </Button>
+            </div>
         );
     }
 
