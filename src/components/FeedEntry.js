4856725c17663aa8e5c62d6a0783c6da5832fb04
rauldevilla <raul.devilla@gmail.com>
2021-02-08 19:17:41 -0500

    Modify FeedEntry

--- src/components/FeedEntry.js
+++ src/components/FeedEntry.js
@@ -5,14 +5,14 @@ import { Configuration } from '../config/Constants';
 class FeedEntry extends Component {
 
     getImageUrl = () => {
-        return this.props.imageUrl ? this.props.imageUrl : Configuration.DEFAULT_IMAGE_URL;
+        return this.props.imageUrl ? <img src={this.props.imageUrl} alt="" /> : <div id="feed-entry-no-img-div"/>;
     }
 
     render() {
         return (
             <div id="feed-entry">
                 <div id="feed-entry-header-container">
-                    <div id="feed-entry-header-container-left"><img src={this.getImageUrl()} alt=""/></div>
+                    <div id="feed-entry-header-container-left">{this.getImageUrl()}</div>
                     <div id="feed-entry-header-container-right"><span id="feed-entry-title">{this.props.title}</span></div>
                 </div>
                 <section>{this.props.content}</section>
