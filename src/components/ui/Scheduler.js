385c77008281f78edb4fefa6ad0afaf92b786d6b
rauldevilla <raul.devilla@gmail.com>
2021-02-18 18:45:30 -0500

    Continue working on Scheduler

--- src/components/ui/Scheduler.js
+++ src/components/ui/Scheduler.js
@@ -4,67 +4,83 @@ const TIME_ZONES = [
 
     {
         label: "5 a.m.",
-        value: 5
+        value: 5,
+        status: "unavailable"
     },
     {
         label: "6 a.m.",
-        value: 6
+        value: 6,
+        status: "unavailable"
     },
     {
         label: "7 a.m.",
-        value: 7
+        value: 7,
+        status: "free"
     },
     {
         label: "8 a.m.",
-        value: 8
+        value: 8,
+        status: "free"
     },
     {
         label: "9 a.m.",
-        value: 9
+        value: 9,
+        status: "free"
     },
     {
         label: "10 a.m.",
-        value: 10
+        value: 10,
+        status: "free"
     },
     {
         label: "11 a.m.",
-        value: 11
+        value: 11,
+        status: "free"
     },
     {
         label: "12 m.",
-        value: 12
+        value: 12,
+        status: "free"
     },
     {
         label: "1 p.m.",
-        value: 13
+        value: 13,
+        status: "free"
     },
     {
         label: "2 p.m.",
-        value: 14
+        value: 14,
+        status: "free"
     },
     {
         label: "3 p.m.",
-        value: 15
+        value: 15,
+        status: "unavailable"
     },
     {
         label: "4 p.m.",
-        value: 16
+        value: 16,
+        status: "unavailable"
     },
     {
         label: "5 p.m.",
-        value: 17
+        value: 17,
+        status: "unavailable"
     },
     {
         label: "6 p.m.",
-        value: 18
+        value: 18,
+        status: "unavailable"
     },
     {
         label: "7 p.m.",
-        value: 19
+        value: 19,
+        status: "free"
     },
     {
-        label: "8 a.m.",
-        value: 20
+        label: "9 a.m.",
+        value: 20,
+        status: "free"
     },
 
 ];
@@ -84,13 +100,22 @@ class Scheduler extends Component {
 
 class TimeZone extends Component {
 
+    getClassNameFromStatus = () => {
+        if ("unavailable" === this.props.timeZone.status) {
+            return "scheduler-component-time-zone-right-unavailable"
+        } if ("free" === this.props.timeZone.status) {
+            return "scheduler-component-time-zone-right-free"
+        }
+        return "";
+    }
+
     render() {
         return (
             <div id="scheduler-component-time-zone">
                 <div id="scheduler-component-time-zone-left">
                     {this.props.timeZone.label}
                 </div>
-                <div id="scheduler-component-time-zone-right-unavailable"></div>
+                <div className={this.getClassNameFromStatus()}></div>
             </div>
         );
     }
